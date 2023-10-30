b368dcc66680dcbeae305db99245daa471230164
ArunArangan <arundark48@gmail.com>
2023-10-30 18:32:13 +0530

    added attribute

--- App.js
+++ App.js
@@ -1,3 +1,7 @@
-const heading = React.createElement("div", {}, "Hello world! from react");
+const heading = React.createElement(
+  "div",
+  { id: "heading" },
+  "Hello world! from react"
+);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
