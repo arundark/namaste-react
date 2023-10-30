386cd2d74ba4d6184295a13dd3d1d069c1a13401
ArunArangan <arundark48@gmail.com>
2023-10-30 18:58:49 +0530

    created sibling elements

--- App.js
+++ App.js
@@ -4,15 +4,16 @@
 //   "Hello world! from react"
 // );
 
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement(
-    "div",
-    { id: "child" },
-    React.createElement("h1", {}, "iam a h1 tag")
-  )
-);
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "iam a h1 tag"),
+    React.createElement("h2", {}, "iam a h2 tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "iam a h1 tag"),
+    React.createElement("h2", {}, "iam a h2 tag"),
+  ]),
+]);
 console.log(parent);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
