17385586e1324ee32a6ff78c28e5eb7f4b9a932d
ArunArangan <arundark48@gmail.com>
2023-10-30 18:52:38 +0530

    nested elements created

--- App.js
+++ App.js
@@ -1,7 +1,18 @@
-const heading = React.createElement(
+// const heading = React.createElement(
+//   "div",
+//   { id: "heading" },
+//   "Hello world! from react"
+// );
+
+const parent = React.createElement(
   "div",
-  { id: "heading" },
-  "Hello world! from react"
+  { id: "parent" },
+  React.createElement(
+    "div",
+    { id: "child" },
+    React.createElement("h1", {}, "iam a h1 tag")
+  )
 );
+console.log(parent);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(parent);
